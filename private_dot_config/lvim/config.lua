175579c67a822f61f07a65ef09314baf9c2629ac
Amin El-Naggar <amin@elnaggar.ca>
2023-02-11 18:58:12 -0500

    hopefully last fix

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -131,7 +131,6 @@ lvim.builtin.which_key.mappings["S"]= {
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
